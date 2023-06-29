04b385279597fedf84d82372be05c6c945970240
YunusEmreKaymak <y.e.imp@hotmail.com>
2023-06-29 22:45:33 +0300

    Project Initialized

---
+++ src/main/java/com/yunus/udemycourcesbackend/UdemyCourcesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yunus.udemycourcesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyCourcesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyCourcesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yunus/udemycourcesbackend/UdemyCourcesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunus.udemycourcesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyCourcesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
