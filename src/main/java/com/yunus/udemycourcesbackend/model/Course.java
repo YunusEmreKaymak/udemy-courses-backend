12c3dcf105d2385766cbf9854dd3d25f44803090
YunusEmreKaymak <y.e.imp@hotmail.com>
2023-07-01 09:03:37 +0300

    Generic Generator removed

--- src/main/java/com/yunus/udemycourcesbackend/model/Course.java
+++ src/main/java/com/yunus/udemycourcesbackend/model/Course.java
@@ -1,17 +1,12 @@
 package com.yunus.udemycourcesbackend.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import org.hibernate.annotations.GenericGenerator;
+import jakarta.persistence.*;
 
 @Entity
 public class Course {
 
     @Id
-    @GeneratedValue(generator = "UUID")
-    @GenericGenerator(name = "UUID")
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     @Column
     private String name;
