ba51fb7c7efcc23de5b3e8f81fba527db2c93f36
YunusEmreKaymak <y.e.imp@hotmail.com>
2023-07-01 17:51:16 +0300

    Controller created and CRUD methods added

---
+++ src/main/java/com/yunus/udemycourcesbackend/api/CourseController.java
@@ -0,0 +1,40 @@
+package com.yunus.udemycourcesbackend.api;
+
+import com.yunus.udemycourcesbackend.dto.CourseDto;
+import com.yunus.udemycourcesbackend.model.Course;
+import com.yunus.udemycourcesbackend.service.CourseService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/course")
+@CrossOrigin("*")
+public class CourseController {
+
+    private CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping()
+    public List<CourseDto> getCourses() {
+        return courseService.getAllCourses();
+    }
+
+    @PostMapping()
+    public void addCourse(@RequestBody Course course){
+        courseService.addCourse(course);
+    }
+
+    @GetMapping("/{name}")
+    public CourseDto getCourseByName(@PathVariable("name") String name){
+        return courseService.getCourseByName(name);
+    }
+
+    @DeleteMapping("{name}")
+    public Boolean deleteCourseByName(@PathVariable("name") String name) {
+        return courseService.deleteCourseByName(name);
+    }
+}
