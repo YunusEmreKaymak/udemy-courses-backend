adaeeeb1b04bb2d9107f9a8ef3ad8c10b2a0cbd9
YunusEmreKaymak <y.e.imp@hotmail.com>
2023-07-01 09:04:46 +0300

    CourseDtoConverter made bean

--- src/main/java/com/yunus/udemycourcesbackend/dto/CourseDtoConverter.java
+++ src/main/java/com/yunus/udemycourcesbackend/dto/CourseDtoConverter.java
@@ -1,7 +1,9 @@
 package com.yunus.udemycourcesbackend.dto;
 
 import com.yunus.udemycourcesbackend.model.Course;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CourseDtoConverter {
     public CourseDto convert(Course course){
         return new CourseDto(course.getName(), course.getOwnerName(), course.getPrice(), course.getImageUrl(), course.getRate());
